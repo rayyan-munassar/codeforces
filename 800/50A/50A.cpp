13e334f9dd5a72a69fef3210ae16ed9b75a6bd78
rayyan-munassar <rayyan3353@gmail.com>
2026-05-21 11:34:17 +0300

    solution 50A

---
+++ 800/50A/50A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+#define pb push_back
+#define mp make_pair
+
+void solve() {
+    
+    int m, n;
+
+    cin >> m >> n;
+
+    int size = m * n;
+
+    cout << std::floor(size / 2);
+}
+
+int main() {
+    fast_io;
+    int t = 1;
+    // cin >> t; // Comment this out if the problem has only 1 testcase
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
