901dcc1bb634f58303e8affda074579e1ab1fa75
rayyan-munassar <rayyan3353@gmail.com>
2026-05-21 05:37:08 +0300

    solution for 231A

---
+++ 800/231A/231A.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+#define pb push_back
+#define mp make_pair
+
+void solve() {
+    int p;
+
+    cin >> p;
+
+    int res;
+
+    for(int i = 0; i < p; i++){
+        int a, b, c;
+
+        cin >> a >> b >> c;
+
+        if(a + b + c >= 2){
+            res++;
+        }
+
+
+
+    }
+
+        cout << res << endl;
+
+
+
+}
+
+int main() {
+    fast_io;
+    int t = 1;
+    cin >> t; // Comment this out if the problem has only 1 testcase
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
